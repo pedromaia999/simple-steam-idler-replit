24e7033d655d36e0b8ff3ecf23c3445963bfdd04
pedromaia999 <158615952+pedromaia999@users.noreply.github.com>
2024-03-01 22:20:01 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const steamTotp = require('steam-totp');
 const keep_alive = require('./keep_alive.js')
 
 var username = 'mtozi@walla.co.il';
-var password = 'Pedromaia123@';
+var password = '';
 var shared_secret = process.env.shared;
 
 var games = [414700,10,730,30,40,70,130,50,60,20,847370,346900,1222670,714010,386360,304930,552990,444090,933110,1049590,1128810,459820,1625450,1714040,1623660,397900,1961460,1335200,1353300,440,1172470,570];  // Enter here AppIDs of the needed games
